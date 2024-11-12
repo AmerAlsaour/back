3337e6c7fca89949ba45505e673749ac39bf53da
Amer Alsaour <154732279+AmerAlsaour@users.noreply.github.com>
2024-11-12 15:12:24 +0300

    edite vercle

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const authRouter = require("./routes/auth");
+const tutorsRouter = require("./routes/teachers");
+const studentRouter = require("./routes/student");
+const { default: mongoose } = require("mongoose");
+app.use(parser.json({ limit: "50mb" }));
+app.use("/images", express.static("images"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/auth", authRouter);
+app.use("/tutors", tutorsRouter);
+app.use("/student", studentRouter);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const message = error.message;
+  const status = error.statusCode || 500;
+  res.status(status).json({
+    Message: message,
+  });
+});
+const port =3000 || process.env.PORT;
+mongoose
+  .connect(
+    "mongodb+srv://ahmadafif:123123123@teachers.cexj3fe.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((a) => {
+    app.listen(port,()=>{
+      console.log("connected");
+    });
+  })
+  .catch((e) => {
+    console.log('error in conection'+e);
+  });
